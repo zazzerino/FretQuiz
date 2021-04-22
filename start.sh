9ae5b62e703566bb79215493e781780107d45d06
Kyle Patterson <patterkyle@gmail.com>
2021-04-22 17:23:19 -0500

    ...

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@
 set -euo pipefail
 
 echo "running podman containers"
-podman run -d --name fq-back -p 3000:3000 fretquiz-backend
-podman run -d --name fq-front -p 8080:80 fretquiz-frontend
+# podman run -d --name fq-back -p 3000:3000 fretquiz-backend
+# podman run -d --name fq-front -p 8080:80 fretquiz-frontend
+podman run -d --network fq-net --name fq-back -p 3000:3000 fretquiz-backend
+podman run -d --network fq-net --name fq-front -p 8080:80 fretquiz-frontend
