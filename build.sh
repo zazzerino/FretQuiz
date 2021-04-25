91681f810e6eff6d38e891b76afdb1564b5e9603
Kyle Patterson <patterkyle@gmail.com>
2021-04-25 11:56:30 -0500

    ...

--- build.sh
+++ build.sh
@@ -16,8 +16,21 @@ npm run build
 
 cd ../..
 
-echo "copying build to /var/www/fretquiz"
+echo "copying uberjar to /opt/fretquiz"
+sudo cp backend/fretquiz/target/FretQuiz.jar /opt/fretquiz/
+
+echo "copying frontend build to /var/www/fretquiz"
 sudo cp -rT frontend/fretquiz/build /var/www/fretquiz
 
 echo "copying nginx conf to /etc/nginx/conf.d"
 sudo cp fretquiz.conf /etc/nginx/conf.d
+
+echo "copying backend service file to /etc/systemd/system"
+sudo cp fretquiz-backend.service /etc/systemd/system
+
+echo "starting backend systemd service"
+sudo systemctl daemon-reload
+sudo systemctl start fretquiz-backend.service
+
+echo "restarting nginx"
+sudo systemctl restart nginx
